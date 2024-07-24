645acac3a139f10da558660ae2965e85345d306c
Stuart Longland <me@vk4msl.com>
2024-07-24 20:17:25 +1000

    threadpool: Add a thread pool singleton class

---
+++ websstv/threadpool.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""
+Shared Thread pool
+"""
+
+# © Stuart Longland VK4MSL
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+from multiprocessing import cpu_count
+from concurrent.futures import ThreadPoolExecutor
+
+
+class ThreadPool(object):
+
+    _instance = None
+
+    @classmethod
+    def get_instance(cls, theads=None):
+        if cls._instance is None:
+            if threads is None:
+                threads = cpu_count()
+
+            cls._instance = ThreadPoolExecutor(threads)
+
+        return cls._instance
