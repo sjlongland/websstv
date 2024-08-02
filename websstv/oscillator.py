dd9aa0800fc4269301e7ba8ba8fb6ea05899e146
Stuart Longland <me@vk4msl.com>
2024-08-02 21:59:55 +1000

    oscillator: Support indefinite-length tuning tones
    Allow generation of a tone for as long as needed by the operator for
    manual-tuning purposes.

--- websstv/oscillator.py
+++ websstv/oscillator.py
@@ -11,6 +11,7 @@ from math import cos, pi
 class SampledTimebase(object):
     def __init__(self, sample_rate):
         self._sample_rate = sample_rate
+        self._indefinite = False
 
     @property
     def sample_rate(self):
@@ -25,15 +26,43 @@ class SampledTimebase(object):
         """
         return int((duration * self.sample_rate) + 0.5)
 
-    def timerange(self, duration, start=0.0):
+    def timeof(self, sample):
+        """
+        Return the time in seconds since the start of the recording epoch
+        for the given sample.
+        """
+        return float(sample) / float(self.sample_rate)
+
+    def timerange(self, duration=None, start=0.0, tail=0.0):
         """
         Return the time samples for each timestamp over the specified
         duration.  ``start`` is assumed to be aligned with a sample.
+
+        If ``tail`` is given, this specifies the time duration after receiving
+        a stop signal to continue emitting samples for (for pulse shaping
+        purposes).
         """
+        if duration is None:
+            self._indefinite = True
+            tail_n = self.samples(tail)
+
         step = 1 / float(self.sample_rate)
 
-        for n in range(self.samples(duration)):
-            yield start + (float(n) * step)
+        if duration is None:
+            n = 0
+            while (self._indefinite is True) or (tail_n > 0):
+                yield start + (float(n) * step)
+                n += 1
+                tail_n -= 1
+        else:
+            for n in range(self.samples(duration)):
+                yield start + (float(n) * step)
+
+    def stop(self):
+        """
+        Stop an indefinite time series.
+        """
+        self._indefinite = False
 
 
 class PulseShaper(SampledTimebase):
@@ -41,7 +70,7 @@ class PulseShaper(SampledTimebase):
     def _sample(time):
         return (1 + cos(pi * time)) / 2
 
-    def pulse(self, duration, risetime=0.01, falltime=None):
+    def pulse(self, duration=None, risetime=0.01, falltime=None):
         # Set a default amplitude to handle the no-rise-time case
         amplitude = 1.0
 
@@ -54,20 +83,26 @@ class PulseShaper(SampledTimebase):
         else:
             fall_samples = self.samples(falltime)
 
-        total_samples = self.samples(duration)
+        if duration is not None:
+            total_samples = self.samples(duration)
 
-        # Ensure we've got enough time for rise/fall
-        slope_samples = rise_samples + fall_samples
-        if total_samples < slope_samples:
-            # There isn't enough time… so we won't reach full amplitude.
-            # The difference in periods gives us the rise/fall time we have.
-            diff = slope_samples - total_samples
-            rise_crop = int((diff / 2) + 0.5)
-            fall_crop = diff - rise_crop
+            # Ensure we've got enough time for rise/fall
+            slope_samples = rise_samples + fall_samples
+            if total_samples < slope_samples:
+                # There isn't enough time… so we won't reach full amplitude.
+                # The difference in periods gives us the rise/fall time we have.
+                diff = slope_samples - total_samples
+                rise_crop = int((diff / 2) + 0.5)
+                fall_crop = diff - rise_crop
 
-            rise_skip = rise_samples - rise_crop
-            fall_skip = fall_samples - fall_crop
+                rise_skip = rise_samples - rise_crop
+                fall_skip = fall_samples - fall_crop
+            else:
+                rise_skip = 0
+                fall_crop = fall_samples
+                fall_skip = 0
         else:
+            # It's an indefinite pulse.
             rise_skip = 0
             fall_crop = fall_samples
             fall_skip = 0
@@ -75,17 +110,23 @@ class PulseShaper(SampledTimebase):
         # Yield the rise samples
         for amplitude in self.rise(risetime, skip=rise_skip):
             yield amplitude
-            total_samples -= 1
+            if duration is not None:
+                total_samples -= 1
 
         # Hold the amplitude at whatever the rise got to
-        while total_samples > fall_crop:
-            yield amplitude
-            total_samples -= 1
+        if duration is None:
+            while self._indefinite:
+                yield amplitude
+        else:
+            while total_samples > fall_crop:
+                yield amplitude
+                total_samples -= 1
 
         # Yield the fall samples
         for amplitude in self.fall(falltime, skip=fall_skip):
             yield amplitude
-            total_samples -= 1
+            if duration is not None:
+                total_samples -= 1
 
     def fall(self, duration, skip=0):
         samples = self.samples(duration)
@@ -156,12 +197,12 @@ class Oscillator(object):
         """
         return self._shaper.samples(duration)
 
-    def timerange(self, duration, start=0.0):
+    def timerange(self, duration=None, start=0.0, tail=0.0):
         """
         Return the timestamps for every sample from the start point to the
         duration given.
         """
-        return self._shaper.timerange(duration, start)
+        return self._shaper.timerange(duration, start, tail)
 
     def silence(self, duration):
         """
@@ -170,12 +211,19 @@ class Oscillator(object):
         for n in range(self.samples(duration)):
             yield self._type(0)
 
-    def generate(self, frequency, duration, risetime=0.06, falltime=None):
+    def generate(
+        self, frequency, duration=None, risetime=0.06, falltime=None
+    ):
         """
         Return the specified duration of tone, at the given frequency.
         """
+        if falltime is None:
+            tail = risetime
+        else:
+            tail = falltime
+
         for time, envelope in zip(
-            self.timerange(duration),
+            self.timerange(duration, tail=tail),
             self._shaper.pulse(duration, risetime, falltime),
         ):
             phase = 2 * pi * frequency * time + self._phase
@@ -186,3 +234,9 @@ class Oscillator(object):
             )
 
         self._phase = phase % (2 * pi)
+
+    def stop(self):
+        """
+        If generating an indefinitely-long tone, stop it now.
+        """
+        self._shaper.stop()
