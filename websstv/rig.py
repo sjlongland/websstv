8d2321bbe9c299488cf733aee94a29f67b7c21ef
Stuart Longland <me@vk4msl.com>
2024-08-03 12:35:03 +1000

    rig: Implement registry interface

--- websstv/rig.py
+++ websstv/rig.py
@@ -11,6 +11,16 @@ underlying interfaces for PTT and rig status control.
 from . import defaults
 from .ptt import init_ptt
 from .hamlib import Rig, parse_enum
+from .registry import Registry
+
+_REGISTRY = Registry(defaults={"type": "basic", "ptt": None})
+
+
+def init_rig(**kwargs):
+    """
+    Initialise a radio interface from the given parameters.
+    """
+    return _REGISTRY.init_instance(**kwargs)
 
 
 def _scale_hz(freq):
@@ -26,13 +36,23 @@ def _scale_hz(freq):
         return "%d Hz" % freq
 
 
+@_REGISTRY.register
 class BasicRig(object):
+    ALIAS = ("basic",)
+
     @classmethod
     def from_cfg(cls, loop=None, log=None, **config):
-        ptt = init_ptt(**config.pop("ptt"), loop=loop, log=log)
+        loop = defaults.get_loop(loop)
+        log = defaults.get_logger(log, cls.__module__)
+
+        ptt_config = config.pop("ptt", None)
+        if ptt_config is not None:
+            ptt = init_ptt(**ptt_config, loop=loop, log=log)
+        else:
+            ptt = None
         return cls(ptt, loop=loop, log=log)
 
-    def __init__(self, ptt, loop=None, log=None):
+    def __init__(self, ptt=None, loop=None, log=None):
         self._loop = defaults.get_loop(loop)
         self._log = defaults.get_logger(log, self.__class__.__module__)
         self._ptt = ptt
@@ -41,8 +61,28 @@ class BasicRig(object):
     def ptt(self):
         return self._ptt
 
+    async def get_freq_unit(self):
+        """
+        Return the VFO frequency scaled to sensible units.
+        """
+        raise NotImplementedError(
+            "Not support by %s" % self.__class__.__name__
+        )
 
+    async def get_s_meter_pts(self):
+        """
+        Return a text-based representation of the S-meter reading
+        as the number of S-points (e.g. "S3", "S9+40dB").
+        """
+        raise NotImplementedError(
+            "Not support by %s" % self.__class__.__name__
+        )
+
+
+@_REGISTRY.register
 class HamlibRig(BasicRig):
+    ALIAS = ("hamlib",)
+
     @classmethod
     def from_cfg(cls, loop=None, log=None, **config):
         loop = defaults.get_loop(loop)
@@ -79,14 +119,6 @@ class HamlibRig(BasicRig):
         """
         return _scale_hz(await self._rig.get_freq(self._vfo))
 
-    async def get_s_meter_db(self):
-        """
-        Return the S-meter reading in dB relative to S9.
-        """
-        return await self._rig.get_level_i(
-            Hamlib.RIG_LEVEL_STRENGTH, self._vfo
-        )
-
     async def get_s_meter_pts(self):
         """
         Return a text-based representation of the S-meter reading
@@ -96,7 +128,7 @@ class HamlibRig(BasicRig):
         # S3=-36, S4=-30, S5=-24, S6=-18, S7=-12, S8=-6, S9=0, +10=10, +20=20,
         # +30=30, +40=40, +50=50 and +60=60.
         # -- https://hamlib.sourceforge.net/manuals/4.3/group__rig.html
-        level = await self.get_s_meter_db()
+        level = await self._get_s_meter_db()
 
         if level <= 0:
             # S9 is 0dB, S0 is -54dB; each S-point in between is 6dB
@@ -105,3 +137,11 @@ class HamlibRig(BasicRig):
         else:
             # dB above S9
             return "S9+%02ddB" % level
+
+    async def _get_s_meter_db(self):
+        """
+        Return the S-meter reading in dB relative to S9.
+        """
+        return await self._rig.get_level_i(
+            Hamlib.RIG_LEVEL_STRENGTH, self._vfo
+        )
