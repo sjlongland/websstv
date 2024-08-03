9b8b7442bfc83286f7e9a31db10b3b65ad98a96c
Stuart Longland <me@vk4msl.com>
2024-08-03 17:41:07 +1000

    observer: Add Slot class, implement disconnect, one-shot

--- websstv/observer.py
+++ websstv/observer.py
@@ -11,27 +11,46 @@ A very simple signalslot work-alike
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 
+class Slot(object):
+    def __init__(self, callback):
+        self._callback = callback
+
+    @property
+    def callback(self):
+        return self._callback
+
+    def __call__(self, *args, **kwargs):
+        self._callback(*args, **kwargs)
+
+    def __eq__(self, other):
+        if other is self:
+            return True
+
+        if isinstance(other, Slot):
+            return self.callback is other.callback
+        else:
+            return self.callback is other
+
+
 class Signal(object):
     def __init__(self):
         self._slots = []
         self._oneshot_slots = []
 
     def connect(self, slot, oneshot=False):
+        if not isinstance(slot, Slot):
+            slot = Slot(slot)
+
         if oneshot:
             self._oneshot_slots.append(slot)
         else:
             self._slots.append(slot)
 
-    def disconnect(self, slot):
-        try:
-            self._slots.remove(slot)
-        except ValueError:
-            pass
+        return slot
 
-        try:
-            self._oneshot_slots.remove(slot)
-        except ValueError:
-            pass
+    def disconnect(self, slot):
+        self._slots = [s for s in self._slots if s != slot]
+        self._oneshot_slots = [s for s in self._oneshot_slots if s != slot]
 
     def emit(self, *args, **kwargs):
         slots = list(self._slots) + list(self._oneshot_slots)
